a5cb8685a0a75713a1ecd7a736098eaed2dbe290
Libor Gabaj <libor.gabaj@gmail.com>
2025-01-09 12:24:37 +0100

    Update code for epoch times and for restoring statistics.

--- src/gbj_appbucket.h
+++ src/gbj_appbucket.h
@@ -18,7 +18,6 @@
 #ifndef GBJ_APPBUCKET_H
 #define GBJ_APPBUCKET_H
 
-#include <Arduino.h>
 #if defined(__AVR__)
   #include <inttypes.h>
 #endif
@@ -28,6 +27,7 @@
 #include "gbj_appstatistics.h"
 #include "gbj_serial_debug.h"
 #include "gbj_timer.h"
+#include <Arduino.h>
 
 #undef SERIAL_PREFIX
 #define SERIAL_PREFIX "gbj_appbucket"
@@ -55,11 +55,13 @@ public:
     parameters.
 
     PARAMETERS:
-    rainfallOffset - Time in minutes from single tip to determine end of a
+
+    rainfallOffset - Time in minutes from last tip to determine end of a
     rainfall.
        - Data type: unsigned integer
        - Default value: none
-       - Limited range: none
+       - Limited range: 0 ~ 255
+
     handlers - A structure with pointers to various callback handler functions.
       - Data type: Handlers
       - Default value: structure with zeroed all handlers
@@ -68,9 +70,9 @@ public:
 
     RETURN: object
   */
-  inline gbj_appbucket(word rainfallOffset, Handlers handlers = Handlers())
+  inline gbj_appbucket(byte rainfallOffset, Handlers handlers = Handlers())
   {
-    rain_.offsetMax = rainfallOffset * 60000;
+    rain_.offsetMax = rainfallOffset * 60;
     handlers_ = handlers;
     timer_ = new gbj_timer(Timing::PERIOD_END);
   }
@@ -89,12 +91,17 @@ public:
   */
   inline void isr()
   {
-    if (millis() - statTime_.getTimeStop() < Timing::PERIOD_DEBOUNCE)
+    // Debouncing
+    if (millis() - rain_.timeLastTip < Timing::PERIOD_DEBOUNCE)
     {
       return;
     }
+    rain_.timeLastTip = millis();
+
+    // Register bucket tip
     rain_.flTips = true;
-    statTime_.set(millis());
+    statTime_.set(rain_.timeBoot +
+                  gbj_apphelpers::convertMs2Sec(rain_.timeLastTip));
     SERIAL_TITLE("ISR")
   }
 
@@ -123,56 +130,131 @@ public:
     }
   }
 
-  // Getters
+  // Set epoch time of MCU boot
+  inline void setTimeBoot(unsigned long timeBoot)
+  {
+    // Set epoch time of MCU boot only once
+    if (rain_.timeBoot == 0)
+    {
+      rain_.timeBoot = timeBoot;
+      // Some bucket tips were collected before the MCU boot
+      if (statTime_.timeStart > 0)
+      {
+        statTime_.timeStart += rain_.timeBoot;
+        statTime_.timeStop += rain_.timeBoot;
+      }
+    }
+  }
+
+  // Return flag about pending rainfall
   inline bool isRain() { return rain_.flPending; }
+
+  // Set flag about pending rainfall
+  inline void setRain(bool flRain) { rain_.flPending = flRain; }
+
+  // Return rainfall duration in seconds
   inline word getRainDuration() { return rain_.duration; }
+
+  // Return rainfall volume in millimeters
   inline float getRainVolume() { return rain_.volume; }
+
+  // Return rainfall rate in millimeters per hour
   inline float getRainRate() { return rain_.rate; }
+
+  // Return rainfall beginning in epoch time in seconds
   inline unsigned long getRainStart() { return statTime_.getTimeStart(); }
+
+  // Return rainfall finishing in epoch time in seconds
   inline unsigned long getRainStop() { return statTime_.getTimeStop(); }
+
+  // Return number of bucket tips
   inline unsigned long getTips() { return statTime_.getCnt(); }
+
+  // Return minimal gap between tips in seconds
   inline unsigned long getTipsGapMin() { return statTime_.getMin(); }
+
+  // Return maximal gap between tips in seconds
   inline unsigned long getTipsGapMax() { return statTime_.getMax(); }
+
+  // Return average gap between tips in seconds
   inline unsigned long getTipsGapAvg() { return statTime_.getAvg(); }
 
+  // Return serialized JSON representation of statistical time data
+  inline String getJsonStatisticTime()
+  {
+    return gbj_appstatistics::exportStatisticTime(statTime_);
+  }
+
+  // Update statistical time data from serialized JSON representation
+  inline bool importStatisticTime(String jsonStr)
+  {
+    return gbj_appstatistics::importStatisticTime(statTime_, jsonStr);
+  }
+
 private:
+  // Timing constants
   enum Timing : word
   {
     // Debouncing delay in milliseconds
-    PERIOD_DEBOUNCE = 1000,
+    PERIOD_DEBOUNCE = 1200,
     // Period for detecting end of rainfall
     PERIOD_END = 5000,
   };
+
+  // Parameters
   enum Params : byte
   {
     // Multiplicator to determine an observation period of rainfall phase
     PARAM_ACTIVE_END_COEF = 5,
   };
+
+  // Rainfall data
   struct Rain
   {
-    // Maximal delay from recent tip determining rainfall end
+    // Epoch time of the MCU boot in seconds
+    unsigned long timeBoot;
+
+    // Timestamp of the last tip in milliseconds
+    unsigned long timeLastTip;
+
+    // Maximal delay from recent tip determining rainfall end in seconds
     unsigned long offsetMax;
+
     // Overall rain time in seconds
     unsigned long duration;
+
     // Millimeters
     float volume;
+
     // Rain intensity in millimeters per hour
     float rate;
+
     // Flag about pending rainfall
     bool flPending;
+
     // Flag about new tips
     bool flTips;
+
+    // Reset all values
     void reset()
     {
       duration = volume = rate = 0;
       flPending = flTips = false;
     }
   } rain_;
+
   // Rain millimeters per bucket tick
   const float BUCKET_FACTOR = 0.2794;
+
+  // Callback handlers
   Handlers handlers_;
+
+  // Statistical time data object
   StatisticTime statTime_ = StatisticTime();
+
+  // Internal timer actuator
   gbj_timer *timer_;
+
   /*
     Rain evaluation.
 
@@ -190,6 +272,7 @@ private:
     {
       return;
     }
+
     // Register rainfall start
     if (!rain_.flPending)
     {
@@ -200,9 +283,10 @@ private:
         handlers_.onRainfallStart();
       }
     }
-    // Evaluate
+
+    // Evaluate rainfall
     rain_.volume = float(statTime_.getCnt() * BUCKET_FACTOR);
-    rain_.duration = gbj_apphelpers::convertMs2Sec(statTime_.get());
+    rain_.duration = statTime_.get();
     rain_.rate = 0;
     if (rain_.duration > 0)
     {
@@ -215,11 +299,14 @@ private:
     SERIAL_VALUE("tipGapMin", getTipsGapMin())
     SERIAL_VALUE("tipGapMax", getTipsGapMax())
     SERIAL_VALUE("tipGapAvg", getTipsGapAvg())
+
+    // Process pending rainfall
     if (handlers_.onRainfallRun != nullptr)
     {
       handlers_.onRainfallRun();
     }
   }
+
   /*
     Rain end detection.
 
@@ -233,15 +320,27 @@ private:
   */
   void rainfallEnd()
   {
+    // Wait for NTP boot
+    if (rain_.timeBoot == 0)
+    {
+      return;
+    }
+
+    unsigned long epochTime =
+      rain_.timeBoot + gbj_apphelpers::convertMs2Sec(millis());
+
     // Determine pending rainfall end
     if (rain_.flPending)
     {
-      unsigned long offsetLimit =
-        min(statTime_.getMax() * Params::PARAM_ACTIVE_END_COEF, rain_.offsetMax);
-      if (millis() - statTime_.getTimeStop() >= offsetLimit)
+      unsigned long offsetLimit = min(
+        statTime_.getMax() * Params::PARAM_ACTIVE_END_COEF, rain_.offsetMax);
+      if (offsetLimit == 0)
+      {
+        offsetLimit = rain_.offsetMax;
+      }
+      if (epochTime - statTime_.getTimeStop() >= offsetLimit)
       {
         SERIAL_VALUE("Rainfall", "STOP")
-        SERIAL_VALUE("offsetLimit", offsetLimit)
         rain_.flPending = false;
         if (handlers_.onRainfallStop != nullptr)
         {
@@ -251,16 +350,16 @@ private:
         rain_.reset();
       }
     }
+
     // No rainfall
     else
     {
       // Close rainfall at single tip and after expiry time
       if (statTime_.getCnt() == 1 &&
-          millis() - statTime_.getTimeStop() > rain_.offsetMax)
+          epochTime - statTime_.getTimeStop() > rain_.offsetMax)
       {
         statTime_.reset();
       }
-      return;
     }
   }
 };
